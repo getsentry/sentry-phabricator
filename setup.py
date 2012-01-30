817d13987e6c3167141b3e12ea30013e68c019f3
David Cramer <dcramer@gmail.com>
2012-01-30 12:30:41 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ tests_require = [
 ]
 
 install_requires = [
-    'sentry>=2.0.0',
+    'sentry>=2.1.0',
     'python-phabricator',
     # https://github.com/disqus/python-phabricator/zipball/master
 ]
 
 setup(
     name='sentry-phabricator',
-    version='0.3.0',
+    version='0.4.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry-phabricator',
